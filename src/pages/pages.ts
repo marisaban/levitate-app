9c2fdd6a0c46c286f8e57307fbbae5b6d1f76135
Maria Isabel Bances <marisaban7@gmail.com>
2018-03-27 18:14:22 -0700

    added icons from client and added them to account page

--- src/pages/pages.ts
+++ src/pages/pages.ts
@@ -19,8 +19,8 @@ import { SelectTokenAmount } from './select-token-modal/select-token-modal';
 import { ShareBrandPage } from './share-brand/share-brand';
 
 // The page the user lands on after opening the app and without a session
-export const FirstRunPage = LaunchPage;
+export const FirstRunPage = UserAccountPage;
 
 // The main page the user will see as they use the app over a long period of time.
 // Change this if not using tabs
-export const MainPage = LaunchPage;
+export const MainPage = UserAccountPage;
